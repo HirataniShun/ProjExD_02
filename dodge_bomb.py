3d0f4f8a5990832400e6b0a5e471a6fc7d7d94e0
C0A22121 <c0a22121a0@edu.teu.ac.jp>
2023-11-28 14:56:28 +0900

    練習5　衝突判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -47,6 +47,10 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
+        if kk_rct.colliderect(bb_rct):
+            print("Game Over")
+            return
         
         key_lst = pg.key.get_pressed()  #練習3
         sum_mv = [0, 0]
