b50d6104a978800113d23278008104e1add551e7
C0A22121 <c0a22121a0@edu.teu.ac.jp>
2023-11-28 14:13:36 +0900

    練習２爆弾移動

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,11 +13,12 @@ def main():
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     bb_img = pg.Surface((20, 20))   #練習１　透明の四角（surface）
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
-    bb_img.set_colorkey((0, 0, 0))
-    bb_rct = bb_img.get_rect()
-    bb_rct.centerx = random.randint(0, WIDTH)
-    bb_rct.centery = random.randint(0, HEIGHT)
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  #練習1
+    bb_img.set_colorkey((0, 0, 0))  #練習1
+    bb_rct = bb_img.get_rect()  #練習1
+    bb_rct.centerx = random.randint(0, WIDTH)  #練習1
+    bb_rct.centery = random.randint(0, HEIGHT)  #練習1
+    vx, vy = +5, +5  #練習2
 
     clock = pg.time.Clock()
     tmr = 0
@@ -28,10 +29,11 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bb_img, bb_rct)
+        bb_rct.move_ip(vx, vy)    #練習2
+        screen.blit(bb_img, bb_rct)  #練習1
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
