723a6524bd37f00576daa57381ffdad5f7777c39
C0A22121 <c0a22121a0@edu.teu.ac.jp>
2023-11-28 14:42:36 +0900

    練習３　こうかとん移動

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,6 +5,13 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1600, 900
 
+delta = {  # 練習3
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0)
+}
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,13 +19,15 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    bb_img = pg.Surface((20, 20))   #練習１　透明の四角（surface）
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  #練習1
-    bb_img.set_colorkey((0, 0, 0))  #練習1
-    bb_rct = bb_img.get_rect()  #練習1
-    bb_rct.centerx = random.randint(0, WIDTH)  #練習1
-    bb_rct.centery = random.randint(0, HEIGHT)  #練習1
-    vx, vy = +5, +5  #練習2
+    kk_rct = kk_img.get_rect()  # 練習3 
+    kk_rct.center = 900, 400  # 練習3
+    bb_img = pg.Surface((20, 20))   # 練習1　透明の四角（surface）
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 練習1
+    bb_img.set_colorkey((0, 0, 0))  # 練習1
+    bb_rct = bb_img.get_rect()  # 練習1
+    bb_rct.centerx = random.randint(0, WIDTH)  # 練習1
+    bb_rct.centery = random.randint(0, HEIGHT)  # 練習1
+    vx, vy = +5, +5  # 練習2
 
     clock = pg.time.Clock()
     tmr = 0
@@ -26,11 +35,20 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        
+        key_lst = pg.key.get_pressed()  #練習3
+        sum_mv = [0, 0]
+        for k, tpl in delta.items():
+            if key_lst[k]:
+                sum_mv[0] += tpl[0]
+                sum_mv[1] += tpl[1]
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        bb_rct.move_ip(vx, vy)    #練習2
-        screen.blit(bb_img, bb_rct)  #練習1
+        bb_rct.move_ip(vx, vy)  # 練習2
+        screen.blit(bb_img, bb_rct)  # 練習1
+        kk_rct.move_ip(sum_mv[0], sum_mv[1])  # 練習3
+        screen.blit(kk_img, kk_rct)  # 練習3
         pg.display.update()
         tmr += 1
         clock.tick(50)
